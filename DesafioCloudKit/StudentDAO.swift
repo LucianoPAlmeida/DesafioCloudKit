d2f6670d6e018f51a2d119094615751804a91e8d
LucianoAlmeida <passos.luciano@outlook.com>
2016-08-26 00:28:25 -0300

    initial commit

---
+++ DesafioCloudKit/StudentDAO.swift
@@ -0,0 +1,13 @@
+//
+//  StudentDAO.swift
+//  DesafioCloudKit
+//
+//  Created by Luciano Almeida on 5/1/16.
+//  Copyright © 2016 Luciano Almeida. All rights reserved.
+//
+
+import UIKit
+
+class StudentDAO: NSObject {
+
+}
